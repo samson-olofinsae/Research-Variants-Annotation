d60a78293c6bbaf719ec4cf87ef37fbe6f961e38
Samson Olofinsae <126152127+samson-olofinsae@users.noreply.github.com>
2025-02-27 10:10:42 +0000

    Update Mutation_table_generator.sh

--- Mutation_table_generator.sh
+++ Mutation_table_generator.sh
@@ -5,7 +5,6 @@
 ## The output can be used as an input for analysis such a non-synonymous/synonymous mutation cancer driver analysis, e.g., dndscv (Martincorena et al., 2017).
 
 #######################################
-
 # INDEL ANALYSIS
 
 # step1 
